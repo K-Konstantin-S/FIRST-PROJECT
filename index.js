c20118862b1b52bf3a9941ba6609accddbec51ec
K-Konstantin-S <koskarlov2008@yandex.ru>
2024-07-22 16:17:19 +0400

    Init

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log(101);
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+button.addEventListener('click', function(){
+    output.innerHTML = 'lol kek';
+});
